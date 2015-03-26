9e59421f2390b89240aae74f08a046ae6c6f9185
ruelle <ruelle@ruelle-System-Product-Name.(none)>
2015-03-26 20:04:24 +0100

    Added header file for 2 color up/down buttons

---
+++ svg2updn.h
@@ -0,0 +1,37 @@
+#ifndef SVG2UPDN_H
+#define SVG2UPDN_H
+
+#include "svg2btn.h"
+
+#include <QTimer>
+
+class Svg2updn : public Svg2btn
+{
+    Q_OBJECT
+public:
+    Svg2updn( int w, int h, QWidget* parent = 0 );
+    Svg2updn( const QByteArray &svgData, int w, int h, QWidget* parent = 0 );
+    Svg2updn( const QString &svgName, int w, int h, QWidget* parent = 0 );
+    void setRange( int min, int max );
+public slots:
+    void setValue( int v );
+    void timerslot();
+    void onClick( int id );
+    void onRelease( int id );
+signals:
+    void valueChanged( int value );
+protected:
+    virtual QString valueToString( int v );
+    virtual int calcSpeed( int r );
+    virtual int calcInterval( int r );
+private:
+    int min;
+    int max;
+    int value;
+    int direction;
+    int rounds;
+    QTimer timer;
+    void init();
+};
+
+#endif
